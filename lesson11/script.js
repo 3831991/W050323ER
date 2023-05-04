045ec85c207a1213025a0d7110389eff7c1c5c97
Your Name <you@example.com>
2023-05-04 11:14:41 +0300

    added

--- lesson11/script.js
+++ lesson11/script.js
@@ -13,3 +13,16 @@ function multi50() {
 
     alert(n * 50);
 }
+
+function multi() {
+    const n1 = document.getElementById("num2").value;
+    const n2 = document.getElementById("num3").value;
+
+    document.getElementById("output1").innerHTML = n1 * n2;
+}
+
+function changeColor() {
+    const color = document.getElementById("myColor").value;
+
+    document.body.style.backgroundColor = color;
+}
