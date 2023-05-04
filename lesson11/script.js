bc73c0371d4d2337322a959f2b435e28497bb70d
Your Name <you@example.com>
2023-05-04 10:58:22 +0300

    added

--- lesson11/script.js
+++ lesson11/script.js
@@ -7,3 +7,9 @@ function welcome2() {
     const userName = document.getElementById("userName").value;
     alert("ברוך הבא ל" + userName);
 }
+
+function multi50() {
+    const n = document.getElementById("num1").value;
+
+    alert(n * 50);
+}
