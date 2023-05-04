b667d910711685b5adac920d894d2b43947a9396
Your Name <you@example.com>
2023-05-04 16:04:44 +0300

    added

--- lesson11/part2/script.js
+++ lesson11/part2/script.js
@@ -1,4 +1,5 @@
 let counter = 1;
+let counter2 = 1;
 
 function task1() {
     document.getElementById("btn").innerHTML = ++counter;
@@ -24,4 +25,24 @@ function task3() {
         document.getElementById("output2").innerHTML = "שקר וכזב";
         document.getElementById("city").style.border = "3px solid red";
     }
+}
+
+function task4() {
+    document.getElementById("img1").style.display = 'none';
+    document.getElementById("img2").style.display = 'block';
+}
+
+function task4_b() {
+    document.getElementById("img1").style.display = 'block';
+    document.getElementById("img2").style.display = 'none';
+}
+
+function task5() {
+    counter2++;
+    const font = counter2 * 10;
+    const color = counter2 * 40;
+
+    document.getElementById("btn2").innerHTML = counter2;
+    document.getElementById("btn2").style.fontSize = `${font}px`;
+    document.getElementById("btn2").style.backgroundColor = `hsl(${color} 100% 38%)`;
 }
