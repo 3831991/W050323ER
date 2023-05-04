5d24e46e6e193e5e35658f9e2127c6c248a70002
Your Name <you@example.com>
2023-05-04 12:42:25 +0300

    added

--- lesson11/part2/script.js
+++ lesson11/part2/script.js
@@ -3,3 +3,13 @@ let counter = 1;
 function task1() {
     document.getElementById("btn").innerHTML = ++counter;
 }
+
+function task2() {
+    const answer = document.getElementById("answer1").value;
+
+    if (answer == "צהוב") {
+        document.getElementById("output1").innerHTML = "נכון";
+    } else {
+        document.getElementById("output1").innerHTML = "תתבייש!!!!";
+    }
+}
