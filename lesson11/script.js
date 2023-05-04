641b5d7367ba712bca7c04e687e4c0680e5cb4a8
Your Name <you@example.com>
2023-05-04 10:20:06 +0300

    added

---
+++ lesson11/script.js
@@ -0,0 +1,9 @@
+function welcome() {
+    alert("ברוכים הבאים");
+    console.log("Welcome to the site");
+}
+
+function welcome2() {
+    const userName = document.getElementById("userName").value;
+    alert("ברוך הבא ל" + userName);
+}
