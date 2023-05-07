7c0151ed074f72fb46ea282603feaa8e466f209d
Your Name <you@example.com>
2023-05-07 15:17:13 +0300

    added

---
+++ lesson12/part2/script.js
@@ -0,0 +1,15 @@
+function sum(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+// function 
+
+const x = sum(5, 6, 9); // 20 
+console.log(x);
+
+const y = sum(10, 6, 9); // 25
+console.log(y);
+
+const z = sum(32, 6, 9); // 47
+console.log(z);
+
