7e9b4e198412a98090722810c8cbfe79702d6787
Your Name <you@example.com>
2023-05-09 12:42:40 +0300

    added

--- lesson13/while/script.js
+++ lesson13/while/script.js
@@ -1,2 +1,19 @@
 const grades = [88, 96, 75, 100, 101, 95, 34, 45, 85];
 
+function sum() {
+    let res = 0;
+
+    let i = 0;
+
+    while (i < grades.length) {
+        res += grades[i];
+
+        i++;
+    }
+
+    document.getElementById("output1").innerHTML = res;
+}
+
+function showAsterisks() {
+    
+}
