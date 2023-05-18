421752e565682577bf34149956103e0cb50e57d9
Your Name <you@example.com>
2023-05-18 11:24:55 +0300

    added

---
+++ lesson16/multi-board/script.js
@@ -0,0 +1,10 @@
+for (let y = 1; y <= 10; y++) {
+    for (let x = 1; x <= 10; x++) {
+        const div = document.createElement("div");
+        div.innerHTML = x * y;
+
+        
+
+        document.getElementById("board").appendChild(div);
+    }
+}
