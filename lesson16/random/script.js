3f0639a3f72426fe074b05df249c3b92cecbd841
Your Name <you@example.com>
2023-05-18 14:25:05 +0300

    added

--- lesson16/random/script.js
+++ lesson16/random/script.js
@@ -1,7 +1,16 @@
 const task1 = document.querySelector('#task1');
+const task2 = document.querySelector('#task2');
+const task3 = document.querySelector('#task3');
 
 task1.querySelector('button').addEventListener('click', function() {
     const rand = Math.random();
     const res = Math.floor(rand * 50) + 1;
     task1.querySelector('p').innerHTML = res;
+});
+
+task2.querySelector('button').addEventListener('click', function() {
+    const max = task2.querySelector("input").value;
+    const rand = Math.random();
+    const res = Math.floor(rand * max) + 1;
+    task2.querySelector('p').innerHTML = res;
 });
