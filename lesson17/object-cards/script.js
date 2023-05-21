0faac69c29c18041a448b233551feb558b76a9a2
Your Name <you@example.com>
2023-05-21 15:57:12 +0300

    added

--- lesson17/object-cards/script.js
+++ lesson17/object-cards/script.js
@@ -7,14 +7,23 @@ const elem = document.querySelector('.row');
 
 for (const student of students) {
     elem.innerHTML += `
-        <div class="col-sm-4 p-2">
+        <div class="col-sm-4 p-2 cardFrame">
             <div class="card">
                 <div class="card-body">
                     <h3>${student.firstName} ${student.lastName}</h3>
                     <p>${student.phone}</p>
                     <p>${student.email}</p>
+                    <button class="btn btn-danger" onclick="remove(this)">מחק</button>
                 </div>
             </div>
         </div>
     `;
+}
+
+function remove(element) {
+    while (!element.className.includes('cardFrame') && element) {
+        element = element.parentElement;
+    }
+
+    element.remove();
 }
