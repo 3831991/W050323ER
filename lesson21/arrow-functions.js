982e6c33d863a257d11daa7b9cdd15f64388d3cf
Your Name <you@example.com>
2023-06-01 14:50:02 +0300

    added

--- lesson21/arrow-functions.js
+++ lesson21/arrow-functions.js
@@ -5,3 +5,32 @@ const fn4 = x => x; // fn4(10) - 10
 const fn5 = () => console.log("banana"); // fn5() - console-log: banana
 const fn6 = () => 12; // fn6() - 12
 const fn7 = x => console.log(x); // fn7(88) - console-log: 88
+
+/********* הדגמה כפונקציות רגילות **********/
+function fun1() {
+
+}
+
+function fun2(a) {
+    
+}
+
+function fun3(a, b) {
+    return a + b;
+}
+
+function fun4(x) {
+    return x;
+}
+
+function fun5() {
+    console.log("banana");
+}
+
+function fun6() {
+    return 12;
+}
+
+function fun7(x) {
+    console.log(x);
+}
