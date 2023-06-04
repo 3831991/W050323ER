6cd37f9024e99474f0ca9fdbfc1fdbe6015e9f92
Your Name <you@example.com>
2023-06-04 14:27:33 +0300

    added

---
+++ lesson22/student-ex/script.js
@@ -0,0 +1,18 @@
+class Student {
+    constructor(student) {
+        this.student = student;
+    }
+
+    student;
+
+    
+}
+
+new Student({
+    firstName: "יוסי",
+    lastName: "אדלר",
+    phone: "059-6506877",
+    birthday: "2004-04-14",
+    city: "חיפה",
+    grades: [90, 80, 100, 95, 100, 100, 100],
+});
