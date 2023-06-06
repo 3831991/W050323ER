46662d8602e78d6d9c4df43eab9cc4ac5205b9f8
Your Name <you@example.com>
2023-06-06 13:01:20 +0300

    added

---
+++ lesson23/fetch/script.js
@@ -0,0 +1,39 @@
+function showStudents(students) {
+    const ol = document.createElement('ol');
+
+    for (const s of students) {
+        const li = document.createElement('li');
+        li.innerHTML = `${s.firstName} ${s.lastName}`;
+
+        ol.appendChild(li);
+    }
+
+    document.querySelector(".frame").appendChild(ol);
+}
+
+/////////////////////// אופציה 1 ////////////////////////
+fetch("students.json")
+    .then(response => response.json())
+    .then(showStudents);
+
+/////////////////////// אופציה 2 ////////////////////////
+// fetch("students.json")
+//     .then(response => response.json())
+//     .then(students => showStudents(students));
+
+/////////////////////// אופציה 3 ////////////////////////
+// כתבנו בפנים ישירות את הפונקציה
+// fetch("students.json")
+//     .then(response => response.json())
+//     .then(students => {
+//         const ol = document.createElement('ol');
+    
+//         for (const s of students) {
+//             const li = document.createElement('li');
+//             li.innerHTML = `${s.firstName} ${s.lastName}`;
+    
+//             ol.appendChild(li);
+//         }
+    
+//         document.querySelector(".frame").appendChild(ol);
+//     });
