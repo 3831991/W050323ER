2ae6446e0faa7cf5764d79128fdafba3d555630a
Your Name <you@example.com>
2023-06-06 09:55:04 +0300

    added

--- lesson23/people/script.js
+++ lesson23/people/script.js
@@ -1,11 +1,11 @@
 class Person {
-    id = null;
-    firstName = '';
-    lastName = '';
-    phone = '';
-    email = '';
-    city = '';
-    birthday = '';
+    id          = null;
+    firstName   = '';
+    lastName    = '';
+    phone       = '';
+    email       = '';
+    city        = '';
+    birthday    = '';
 
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
@@ -13,32 +13,81 @@ class Person {
 
     getPerson() {
         return {
-            id: this.id,
-            firstName: this.firstName,
-            lastName: this.lastName,
-            phone: this.phone,
-            email: this.email,
-            city: this.city,
-            birthday: this.birthday,
+            id:         this.id,
+            firstName:  this.firstName,
+            lastName:   this.lastName,
+            phone:      this.phone,
+            email:      this.email,
+            city:       this.city,
+            birthday:   this.birthday,
         };
     }
 
     getAge() {
         const rest = new Date() - new Date(this.birthday);
-        return Math.floor(rest / (1000 / 60 / 60 / 24 / 365) * 10) / 10;
+        return Math.floor(rest / 1000 / 60 / 60 / 24 / 365 * 10) / 10;
+    }
+
+    constructor(person) {
+        this.id         = person.id;
+        this.firstName  = person.firstName;
+        this.lastName   = person.lastName;
+        this.phone      = person.phone;
+        this.email      = person.email;
+        this.city       = person.city;
+        this.birthday   = person.birthday;
     }
 }
 
 class Student extends Person {
-    
+    grades = [];
+
+    getAverage() {
+
+    }
+
+    getBestGrade() {
+
+    }
+
+    getAmountTests() {
+
+    }
 }
 
-class Teacher {
-
+class Teacher extends Person {
+    salary = 50000;
+    startDate = '';
 }
 
-class Director {
+class Director extends Person { }
 
-}
+const student = new Student({
+    id: 537,
+    firstName: 'Aviv',
+    lastName: 'Shapira',
+    phone: '050-050-0500',
+    email: 'abc@abc.abc',
+    city: 'Kfar haruv',
+    birthday: '2022-01-01',
+});
 
-const student = new Student();
+const teacher = new Teacher({
+    id: 777,
+    firstName: 'Adam',
+    lastName: 'Regev',
+    phone: '050-050-1234',
+    email: 'ocd@ocd.com',
+    city: 'Shaked',
+    birthday: '1997-07-22',
+});
+
+const director = new Director({
+    id: 100,
+    firstName: 'Lars',
+    lastName: 'Shalit',
+    phone: '050-050-9632',
+    email: 'adhd@ocd.com',
+    city: 'Kiryat Arba',
+    birthday: '2003-10-04',
+});
