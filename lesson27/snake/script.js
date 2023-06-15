df17cd1676992eec15328db47f309955198e266e
Your Name <you@example.com>
2023-06-15 10:40:08 +0300

    added

--- lesson27/snake/script.js
+++ lesson27/snake/script.js
@@ -1,7 +1,9 @@
 const height = 40;
-const width = 35;
+const width = 50;
 const snake = [3, 2, 1, 0];
 let head = snake[0];
+let direction = 'left';
+let interval;
 
 const board = document.querySelector(".board");
 board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
@@ -19,9 +21,54 @@ function color() {
     // Get all board elements.
     const divs = document.querySelectorAll('.board div');
 
-    // Remove the class "Active" from all elements.
-    divs.forEach(elem => elem.classList.remove('active'));
+    // Remove all classes.
+    divs.forEach(elem => {
+        elem.classList.remove('active');
+        elem.classList.remove('head');
+        elem.classList.remove('up');
+        elem.classList.remove('right');
+        elem.classList.remove('down');
+        elem.classList.remove('left');
+    });
 
     // Add the class "Active" to the element of the snake.
     snake.forEach(num => divs[num].classList.add('active'));
+
+    divs[head].classList.add('head');
+    divs[head].classList.add(direction);
 }
+
+function move(dir) {
+    if (dir === 'up') {
+        head -= width;
+    } else if (dir === 'down') {
+        head += width;
+    } else if (dir === 'left') {
+        head++;
+    } else if (dir === 'right') {
+        head--;
+    }
+
+    direction = dir;
+    snake.unshift(head);
+    snake.pop();
+    color();
+    startAuto();
+}
+
+function startAuto() {
+    clearInterval(interval);
+    interval = setInterval(() => move(direction), 100);
+}
+
+window.addEventListener('keydown', ev => {
+    ev.preventDefault();
+
+    // Checks which key was pressed.
+    switch (ev.key) {
+        case 'ArrowUp': move('up'); break;
+        case 'ArrowRight': move('right'); break;
+        case 'ArrowDown': move('down'); break;
+        case 'ArrowLeft': move('left'); break;
+    }
+});
