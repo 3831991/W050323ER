b36a723a0d7763a076f1fab726dba89b41019cfc
Your Name <you@example.com>
2023-06-18 12:57:30 +0300

    added

--- lesson28/puzzle/script.js
+++ lesson28/puzzle/script.js
@@ -97,6 +97,28 @@ function checkAllOptions() {
 
     if (isGameOver) {
         board.classList.add('game-over');
-        console.log('Game Over')
+
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            decay: 0.9,
+            origin: { y: 0.6 }
+        });
+
+        const winner = document.createElement("div");
+        winner.classList.add("winner");
+        winner.innerHTML = "ניצחת";
+
+        document.body.appendChild(winner);
+
+        setTimeout(function() {
+            location.reload();
+        }, 5 * 1000);
     }
+}
+
+function cheat() {
+    divs.forEach((elem, i) => elem.innerHTML = i + 1);
+    divs[divs.length - 1].innerHTML = '';
+    checkAllOptions();
 }
