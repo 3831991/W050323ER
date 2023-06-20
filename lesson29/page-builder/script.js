bc296922b1172ea9f31e16bbf81ef4306b95cb80
Your Name <you@example.com>
2023-06-20 12:39:51 +0300

    added

--- lesson29/page-builder/script.js
+++ lesson29/page-builder/script.js
@@ -19,13 +19,17 @@ function changeStyle(key, value) {
 }
 
 document.querySelectorAll("nav a").forEach(a => {
+    // הוספת אירוע לחיצה על התפריט
     a.addEventListener('click', ev => {
+        // לבטל את האקטיב מכולם + הסתרת כל האלמנטים
         document.querySelectorAll("nav a").forEach(el => {
             el.classList.remove('active');
             document.querySelector(el.id).style.display = 'none';
         });
 
+        // צביעת התפריט הנוכחי
         ev.target.classList.add('active');
+        // הצגת האלמנט הנוכחי
         document.querySelector(ev.target.id).style.display = 'block';
     });
 });
