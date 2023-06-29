a5d124d5cac1286761f527cf4aa4e036c304f1cf
Your Name <you@example.com>
2023-06-29 14:36:45 +0300

    added

--- lesson30/script.js
+++ lesson30/script.js
@@ -1,27 +1,36 @@
-let i = 1;
-
+/**
+ * פונקיצה להוספת משימה
+ * @param parentDiv אלמנט שרוצים להוסיף אחריו את המשימה (אופציונלי)
+ */
 function addTask(parentDiv = null) {
     const div = document.createElement('div');
     div.className = "task";
 
+    // מוסיף פסקה לתוך המשימה
     const p = document.createElement('p');
-    p.innerHTML = `Task ${i++}`;
+    // מאפשר לערוך את הפסקה
     p.contentEditable = true;
     div.appendChild(p);
 
+    // לצורך ביטול השורה כשלוחצים על אנטר
     p.addEventListener("keydown", ev => {
         if (ev.key == 'Enter' && !ev.shiftKey) {
             ev.preventDefault();
         }
     });
 
+    // לצורך מעבר עם הסמן בין משימות
     p.addEventListener("keyup", ev => {
+        // האלמנט של המשימה
         const task = ev.target.parentElement;
 
+        // אם לוחצים על החץ למטה ויש אלמנט מתחת - הוא עובר אליו
         if (ev.key == 'ArrowDown' && task.nextSibling) {
             task.nextSibling.querySelector("p").focus();
+        // אם לוחצים על החץ למעלה ויש אלמנט מעל - הוא עובר אליו
         } else if (ev.key == 'ArrowUp' && task.previousSibling) {
             task.previousSibling.querySelector("p").focus();
+        // אם לוחצים על אנטר *ללא* שיפט, אך הוא מוסיף משימה חדשה
         } else if (ev.key == 'Enter' && !ev.shiftKey) {
             addTask(task);
         }
@@ -58,13 +67,21 @@ function addTask(parentDiv = null) {
     btnFrame3.appendChild(btnUndo);
     div.appendChild(btnFrame3);
 
+    // אם קיבלנו אלמנט כפרמטר, משמע שאנחנו רוצים להוסיף אחריו משימה
     if (parentDiv) {
         document.querySelector('.taskList').insertBefore(div, parentDiv.nextSibling);
+    // אחרת אנחנו מוסיפים משימה בסוף
     } else {
         document.querySelector('.taskList').appendChild(div);
     }
 
+    // שם את הסמן בתוך התיבה
     p.focus();
 }
 
+// הוספת משימה ראשונית
 addTask();
+
+function save() {
+    
+}
