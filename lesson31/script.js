d3ad352bded200bb8c9b5303c2585ac2cc815556
Your Name <you@example.com>
2023-07-02 12:51:09 +0300

    added

--- lesson31/script.js
+++ lesson31/script.js
@@ -6,10 +6,25 @@ function login() {
 
     fetch("http://api.shipap.co.il/login", {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(obj),
+    })
+    .then(res => res.json())
+    .then(data => {
+        if (data.status == 'success') {
+            // לכתוב הודעת ברוכים הבאים ליוזר
+        } else {
+            alert(data.message);
+        }
+    });
+}
+
+function loginStatus() {
+    fetch("http://api.shipap.co.il/login", {
+        credentials: 'include',
     })
     .then(res => res.json())
     .then(data => console.log(data))
