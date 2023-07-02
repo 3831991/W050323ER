0f71258563315c34020e9cd5dd3cf97d80420f60
Your Name <you@example.com>
2023-07-02 11:46:40 +0300

    added

--- lesson31/script.js
+++ lesson31/script.js
@@ -1,10 +1,15 @@
-function login(userName, password) {
+function login() {
+    const obj = {
+        userName: document.querySelector("#userName").value,
+        password: document.querySelector("input[type=password]").value,
+    };
+
     fetch("http://api.shipap.co.il/login", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ userName, password }),
+        body: JSON.stringify(obj),
     })
     .then(res => res.json())
     .then(data => console.log(data))
