8e990f65eb58b01c6a94945f9871fc07dff1e45f
Your Name <you@example.com>
2023-07-09 14:25:03 +0300

    added

--- lesson33/hello-react/src/components/Settings.js
+++ lesson33/hello-react/src/components/Settings.js
@@ -4,12 +4,17 @@ import './Settings.css';
 function Settings() {
     const [brightness, setBrightness] = useState(100);
 
+    function changeBrightness(val) {
+        setBrightness(val);
+        document.body.style.filter = `brightness(${val}%)`;
+    }
+
     return (
         <>
             <div className="card">
                 <b>רמת בהירות:</b>
-                <input type="range" min="0" max="100" value={brightness} onChange={ev => setBrightness(ev.target.value)} />
-                <input type="number" min="0" max="100" value={brightness} onChange={ev => setBrightness(ev.target.value)} />
+                <input type="range" min="0" max="100" value={brightness} onChange={ev => changeBrightness(ev.target.value)} />
+                <input type="number" min="0" max="100" value={brightness} onChange={ev => changeBrightness(ev.target.value)} />
             </div>
 
             <div className="card">
