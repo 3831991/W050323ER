2293fd7230dec0df2967769417f2588ca66157bb
Your Name <you@example.com>
2023-07-11 10:19:19 +0300

    added

--- lesson33/hello-react/src/components/Range.js
+++ lesson33/hello-react/src/components/Range.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import './Range.css';
 
 function Range({ title, value, min, max, change }) {
-    const [num, setNum] = useState(value || 100);
+    const [num, setNum] = useState(value);
 
     function changeVal(val) {
         setNum(val);
@@ -10,10 +10,10 @@ function Range({ title, value, min, max, change }) {
 
     return (
         <div className="range">
-            <b>{title || 'ערך מסויים'}:</b>
+            {title ? (<b>{title}:</b>) : ''}
 
-            <input type="range" min={min || 0} max={max || 100} value={num} onChange={ev => changeVal(ev.target.value)} />
-            <input type="number" min={min || 0} max={max || 100} value={num} onChange={ev => changeVal(ev.target.value)} />
+            <input type="range" min={min} max={max} value={num} onChange={ev => changeVal(ev.target.value)} />
+            <input type="number" min={min} max={max} value={num} onChange={ev => changeVal(ev.target.value)} />
         </div>
     );
 }
--- lesson33/hello-react/src/components/Settings.js
+++ lesson33/hello-react/src/components/Settings.js
@@ -4,11 +4,34 @@ import Range from "./Range";
 
 function Settings() {
     const [brightness, setBrightness] = useState(30);
-    const [letterSpacing, letterChange] = useState(10);
+    const [letterSpacing, setLetterSpacing] = useState(10);
+    const [fontSize, setFontSize] = useState(20);
+    const [padding, setPadding] = useState(5);
+    const [invertColor, setInvertColor] = useState(5);
 
     function changeBrightness(val) {
         setBrightness(val);
         document.querySelector('html').style.filter = `brightness(${val}%)`;
+    }
+
+    function changeLetterSpacing(val) {
+        setLetterSpacing(val);
+        
+    }
+
+    function changeFontSize(val) {
+        setFontSize(val);
+
+    }
+
+    function changePadding(val) {
+        setPadding(val);
+
+    }
+
+    function changeInvertColor(val) {
+        setInvertColor(val);
+
     }
 
     return (
@@ -29,7 +52,37 @@ function Settings() {
                     value={letterSpacing}
                     min={10}
                     max={50}
-                    change={val => changeBrightness(val)}
+                    change={val => changeLetterSpacing(val)}
+                />
+            </div>
+
+            <div className="card">
+                <Range 
+                    title="גודל הגופן"
+                    value={fontSize}
+                    min={10}
+                    max={70}
+                    change={val => changeFontSize(val)}
+                />
+            </div>
+
+            <div className="card">
+                <Range 
+                    title="ריווח של הדף"
+                    value={padding}
+                    min={0}
+                    max={60}
+                    change={val => changePadding(val)}
+                />
+            </div>
+
+            <div className="card">
+                <Range 
+                    title="היפוך צבעים"
+                    value={invertColor}
+                    min={0}
+                    max={60}
+                    change={val => changeInvertColor(val)}
                 />
             </div>
         </>
