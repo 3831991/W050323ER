303ea3fe8f5424edc27860d74b248135dab16fbf
Your Name <you@example.com>
2023-07-11 14:48:37 +0300

    added

--- lesson33/hello-react/src/components/Home.js
+++ lesson33/hello-react/src/components/Home.js
@@ -9,9 +9,9 @@ function Home() {
             <Numbers min={30} max={75} />
             <Numbers min={100} max={110} />
 
-            <Square width={40} height={30} bg="blue" />
-            <Square width={60} height={20} bg="green" />
-            <Square width={80} height={50} bg="yellow" />
+            <Square width={400} height={300} bg="blue" />
+            <Square width={600} height={200} bg="green" />
+            <Square width={800} height={500} bg="yellow" />
         </>
     );
 }
--- lesson33/hello-react/src/components/Square.js
+++ lesson33/hello-react/src/components/Square.js
@@ -1,5 +1,7 @@
-function Square() {
-
+function Square({ width, height, bg }) {
+    return (
+        <div style={{ width, height, backgroundColor: bg }}></div>
+    )
 }
 
 export default Square;
