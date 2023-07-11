d7e8f2fe78840012cb40c62ce36aa35bd7225315
Your Name <you@example.com>
2023-07-11 15:12:06 +0300

    added

--- lesson33/hello-react/src/components/Square.js
+++ lesson33/hello-react/src/components/Square.js
@@ -1,7 +1,3 @@
-function Square({ width, height, bg }) {
-    return (
-        <div style={{ width, height, backgroundColor: bg }}></div>
-    )
+export default function Square({ width, height, bg }) {
+    return <div style={{ width, height, backgroundColor: bg }}></div>
 }
-
-export default Square;
