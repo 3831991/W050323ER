173d9f33ca178f32d087b856a98f0993a5a66d3f
Your Name <you@example.com>
2023-07-13 10:01:57 +0300

    added

--- react/src/components/ErrorPage.js
+++ react/src/components/ErrorPage.js
@@ -3,7 +3,7 @@ import './ErrorPage.css';
 export default function ErrorPage() {
     return (
         <div className="error-page">
-            הדף לא קיים
+            <p>הדף לא קיים</p>
         </div>
     )
 }
