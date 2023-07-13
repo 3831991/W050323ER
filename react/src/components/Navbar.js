81222dd5acfd28cf76f008ca149ad30f8ceef72c
Your Name <you@example.com>
2023-07-13 10:33:19 +0300

    added

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -7,6 +7,7 @@ function Navbar() {
                 <li><a href="/">בית</a></li>
                 <li><a href="/counter">מונה</a></li>
                 <li><a href="/gallery">גלריה</a></li>
+                <li><a href="/users">משתמשים</a></li>
                 <li><a href="/settings">הגדרות</a></li>
                 <li><a href="/contact">צור קשר</a></li>
             </ul>
