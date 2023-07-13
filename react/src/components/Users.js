5a8eda5ad99b85d52b527a928580b8f3b8aebb57
Your Name <you@example.com>
2023-07-13 11:52:04 +0300

    added

--- react/src/components/Users.js
+++ react/src/components/Users.js
@@ -1,19 +1,37 @@
 import { useState } from 'react';
 import './Users.css';
 import { users } from './UsersData';
+import moment from 'moment';
 
 export default function Users() {
     const [data, setData] = useState(users);
 
     return (
-        <>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>שם מלא</th>
+                    <th>אימייל</th>
+                    <th>טלפון</th>
+                    <th>תאריך לידה</th>
+                </tr>
+            </thead>
+            <tbody>
             {
-                data.map(user => {
+                data.map((user, i) => {
                     return (
-                        <p>{user.firstName} {user.lastName}</p>
+                        <tr>
+                            <td>{i + 1}</td>
+                            <td>{user.firstName} {user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td>{user.phone}</td>
+                            <td>{moment(user.birthday).format('DD/MM/yyy')}</td>
+                        </tr>
                     )
                 })
             }
-        </>
+            </tbody>
+        </table>
     );
 }
