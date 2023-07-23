6e5d0bf530a1775144e56f8d1ddf8874da9eff59
Your Name <you@example.com>
2023-07-23 12:56:51 +0300

    added

--- react/products/src/components/Products.js
+++ react/products/src/components/Products.js
@@ -31,26 +31,32 @@ export default function Products() {
         <div className="Products">
             <h2>מוצרים</h2>
             
-            <div className='grid'>
-                <div>#</div>
-                <div>שם המוצר</div>
-                <div>מחיר</div>
-                <div>הנחה</div>
-                <div></div>
             {
-                products.map((p, i) => {
-                    return (
-                        <React.Fragment key={p.id}>
-                            <div>{i + 1}</div>
-                            <div>{p.name}</div>
-                            <div>{p.price}</div>
-                            <div>{p.discount}</div>
-                            <div><button className="remove" onClick={() => remove(p.id)}>❌</button></div>
-                        </React.Fragment>
-                    )
-                })
+                products.length ?
+                (
+                    <div className='grid'>
+                        <div>#</div>
+                        <div>שם המוצר</div>
+                        <div>מחיר</div>
+                        <div>הנחה</div>
+                        <div></div>
+                    {
+                        products.map((p, i) => {
+                            return (
+                                <React.Fragment key={p.id}>
+                                    <div>{i + 1}</div>
+                                    <div>{p.name}</div>
+                                    <div>{p.price}</div>
+                                    <div>{p.discount}</div>
+                                    <div><button className="remove" onClick={() => remove(p.id)}>❌</button></div>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                    </div>
+                ) : 
+                <h3 className='noData'>אין נתונים</h3>
             }
-            </div>
         </div>
     )
 }
