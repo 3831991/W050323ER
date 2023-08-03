6a4c1970b1a65cd4ed2d3bc4897090e26f81dd79
Your Name <you@example.com>
2023-08-03 11:45:53 +0300

    added

--- react/news-manage/src/articles/Articles.js
+++ react/news-manage/src/articles/Articles.js
@@ -3,6 +3,7 @@ import './Articles.css';
 import moment from 'moment';
 import { AiFillEdit } from 'react-icons/ai';
 import { BsFillTrash3Fill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export default function Articles() {
     const [articles, setArticles] = useState([]);
@@ -30,14 +31,14 @@ export default function Articles() {
             <tbody>
             {
                 articles.map((art, i) => 
-                    <tr>
+                    <tr key={art.id}>
                         <td>{i + 1}</td>
                         <td>{moment(art.addedTime).format('DD/MM/YY')}</td>
                         <td>{moment(art.publishDate).format('DD/MM')}</td>
                         <td>{art.headline}</td>
                         <td>{art.views}</td>
                         <td>
-                            <button className='green'><AiFillEdit /></button>
+                            <Link to={`/article/${art.id}`}><button className='green'><AiFillEdit /></button></Link>
                             <button className='red'><BsFillTrash3Fill /></button>
                         </td>
                     </tr>
