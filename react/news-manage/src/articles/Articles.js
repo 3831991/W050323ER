a5dc296941b5bedcc70e1363f607b3ad7222c5e8
Your Name <you@example.com>
2023-08-03 11:38:17 +0300

    added

--- react/news-manage/src/articles/Articles.js
+++ react/news-manage/src/articles/Articles.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import './Articles.css';
+import moment from 'moment';
+import { AiFillEdit } from 'react-icons/ai';
+import { BsFillTrash3Fill } from 'react-icons/bs';
 
 export default function Articles() {
     const [articles, setArticles] = useState([]);
@@ -11,21 +14,36 @@ export default function Articles() {
         .then(res => res.json())
         .then(data => setArticles(data));
     }, []);
-
-    // id
-    // addedTime
-    // publishDate
-    // headline
-    // description
-    // views
     
     return (
-        <>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>תאריך הוספה</th>
+                    <th>תאריך פרסום</th>
+                    <th>כותרת</th>
+                    <th>צפיות</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
             {
-                articles.map(art => 
-                    <p>{art.headline}</p>
+                articles.map((art, i) => 
+                    <tr>
+                        <td>{i + 1}</td>
+                        <td>{moment(art.addedTime).format('DD/MM/YY')}</td>
+                        <td>{moment(art.publishDate).format('DD/MM')}</td>
+                        <td>{art.headline}</td>
+                        <td>{art.views}</td>
+                        <td>
+                            <button className='green'><AiFillEdit /></button>
+                            <button className='red'><BsFillTrash3Fill /></button>
+                        </td>
+                    </tr>
                 )
             }
-        </>
+            </tbody>
+        </table>
     )
 }
