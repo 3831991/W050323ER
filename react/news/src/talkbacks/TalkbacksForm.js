2d76fd2403acf9ac2449570a50984938b567f968
Your Name <you@example.com>
2023-08-08 11:53:22 +0300

    added

--- react/news/src/talkbacks/TalkbacksForm.js
+++ react/news/src/talkbacks/TalkbacksForm.js
@@ -4,20 +4,21 @@ import { useState } from 'react';
 
 export default function TalkbacksForm({ articleId, parent }) {
     const [data, setData] = useState([]);
-    const [formData, setFormData] = useState({
-        
-    })
+    const [comment, setComment] = useState('');
 
     const addComment = () => {
         fetch(`https://api.shipap.co.il/articles/${articleId}/talkbacks?token=${TOKEN}`, {
             method: 'POST',
             credentials: 'include',
             headers: {'Content-type': 'application/json'},
-            body: JSON.stringify(formData),
+            body: JSON.stringify({
+                comment,
+                parent,
+            }),
         })
         .then(res => res.json())
         .then(data => {
-            
+
         });
     }
 
