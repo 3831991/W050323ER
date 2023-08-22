4d0a7688f90f6fb669a48759244db15c106f86a2
Your Name <you@example.com>
2023-08-22 09:48:16 +0300

    added

--- react/full-stack-project/src/users/Users.js
+++ react/full-stack-project/src/users/Users.js
@@ -1,18 +1,46 @@
 import './Users.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import moment from 'moment';
 
 export default function Users() {
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:4000/users")
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setUsers(data.slice(0, 50)));
     }, []);
     
     return (
         <div>
             <h2>משתמשים</h2>
 
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>תאריך יצירה</th>
+                        <th>שם פרטי</th>
+                        <th>שם משפחה</th>
+                        <th>אימייל</th>
+                        <th>טלפון</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    users.map((u, i) => 
+                        <tr>
+                            <td>{i + 1}</td>
+                            <td>{moment(u.createdTime).format("DD/MM/YY")}</td>
+                            <td>{u.firstName}</td>
+                            <td>{u.lastName}</td>
+                            <td>{u.email}</td>
+                            <td>{u.phone}</td>
+                        </tr>
+                    )
+                }
+                </tbody>
+            </table>
         </div>
     )
 }
