fd9061d478b88a780cfdb7240a10de64d175ac14
Your Name <you@example.com>
2023-08-22 09:33:07 +0300

    added

--- nodejs/hello-nodejs/app.js
+++ nodejs/hello-nodejs/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./router')(app);
 require('./sqlConnection');
 
 app.use(cors({
@@ -16,3 +15,5 @@ app.use(express.json());
 app.listen(4000, () => {
     console.log("Listening to port 4000...");
 });
+
+require('./router')(app);
