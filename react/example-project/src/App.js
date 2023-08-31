ad4ccc9c188fa415e9a338daf8cd3c37c7dbbb9a
Your Name <you@example.com>
2023-08-31 10:06:15 +0300

    added

---
+++ react/example-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TextField from '@mui/material/TextField';
+
+function App() {
+    return (
+        <div className="App">
+            <TextField id="outlined-basic" label="משהו אחר" variant="outlined" />
+        </div>
+    );
+}
+
+export default App;
