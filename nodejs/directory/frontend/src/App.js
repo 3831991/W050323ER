28dfcced4f50f7de899e82585329f7030ba7a2a8
Your Name <you@example.com>
2023-09-10 15:34:38 +0300

    added

---
+++ nodejs/directory/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1>ניהול תיקיות</h1>
+            <div className="frame">
+                
+            </div>
+        </>
+    );
+}
+
+export default App;
