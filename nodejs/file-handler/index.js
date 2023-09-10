fe55f457bcbe58ef0e2a33a8adbdaa49733c0a47
Your Name <you@example.com>
2023-09-10 15:15:41 +0300

    added

--- nodejs/file-handler/index.js
+++ nodejs/file-handler/index.js
@@ -17,11 +17,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', (req, res) => {
-    fs.appendFile("test.txt", 'Hello world!', (err) => {
+    fs.appendFile("test.txt", 'Hello world!\n', (err) => {
         if (err) {
             throw err;
         }
 
         res.send('File created...');
     });
+});
+
+app.get('/test2', (req, res) => {
+    fs.appendFile("test2.txt", `${req.query.log || 'אין תוכן'}\n`, (err) => {
+        if (err) {
+            throw err;
+        }
+
+        res.send('File created...');
+    });
+});
+
+app.get('/show-file', (req, res) => {
+    res.sendFile(`${__dirname}/test.txt`);
 });
