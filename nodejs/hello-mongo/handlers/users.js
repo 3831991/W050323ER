4e5c79bbbf846d5b1af415bae169b582a0da96ae
Your Name <you@example.com>
2023-09-12 15:30:00 +0300

    added

---
+++ nodejs/hello-mongo/handlers/users.js
@@ -0,0 +1,53 @@
+module.exports = (app, mongoose) => {
+    const schema = new mongoose.Schema({
+        firstName: String,
+        lastName: String,
+        phone: String,
+        email: String,
+    });
+    
+    const User = mongoose.model("users", schema);
+
+    app.get('/', (req, res) => {
+        res.send("Welcome");
+    });
+    
+    app.get('/users', async (req, res) => {
+        res.send(await User.find());
+    });
+    
+    app.get('/users/:id', async (req, res) => {
+        res.send(await User.findOne({ _id: req.params.id }));
+    });
+    
+    app.post('/users', async (req, res) => {
+        const { firstName, lastName, email, phone } = req.body;
+    
+        const user = new User({ firstName, lastName, email, phone });
+        const newUser = await user.save();
+        res.send(newUser);
+    });
+    
+    app.put('/users/:id', async (req, res) => {
+        const { firstName, lastName, email, phone } = req.body;
+        const user = await User.findOne({ _id: req.params.id });
+    
+        if (!user) {
+            return res.status(403).send("משתמש לא קיים!");
+        }
+    
+        user.firstName  = firstName;
+        user.lastName   = lastName;
+        user.email      = email;
+        user.phone      = phone;
+    
+        await user.save();
+    
+        res.send(user);
+    });
+    
+    app.delete('/users/:id', async (req, res) => {
+        await User.deleteOne({ _id: req.params.id });
+        res.send();
+    });
+}
--- nodejs/hello-mongo/index.js
+++ nodejs/hello-mongo/index.js
@@ -9,15 +9,6 @@ async function main() {
 
 main().catch(err => console.log(err));
 
-const schema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    phone: String,
-    email: String,
-});
-
-const User = mongoose.model("users", schema);
-
 const app = express();
 
 app.use(express.json());
@@ -31,45 +22,5 @@ app.use(cors({
 
 app.listen(4444);
 
-app.get('/', (req, res) => {
-    res.send("Welcome");
-});
-
-app.get('/users', async (req, res) => {
-    res.send(await User.find());
-});
-
-app.get('/users/:id', async (req, res) => {
-    res.send(await User.findOne({ _id: req.params.id }));
-});
-
-app.post('/users', async (req, res) => {
-    const { firstName, lastName, email, phone } = req.body;
-
-    const user = new User({ firstName, lastName, email, phone });
-    const newUser = await user.save();
-    res.send(newUser);
-});
-
-app.put('/users/:id', async (req, res) => {
-    const { firstName, lastName, email, phone } = req.body;
-    const user = await User.findOne({ _id: req.params.id });
-
-    if (!user) {
-        return res.status(403).send("משתמש לא קיים!");
-    }
-
-    user.firstName  = firstName;
-    user.lastName   = lastName;
-    user.email      = email;
-    user.phone      = phone;
-
-    await user.save();
-
-    res.send(user);
-});
-
-app.delete('/users/:id', async (req, res) => {
-    await User.deleteOne({ _id: req.params.id });
-    res.send();
-});
+require('./handlers/users')(app, mongoose);
+require('./handlers/grades')(app, mongoose);
