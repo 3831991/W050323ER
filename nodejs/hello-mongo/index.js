bed7329064c57e4491ede6b50fa465bcc1c884dd
Your Name <you@example.com>
2023-09-12 14:48:51 +0300

    added

--- nodejs/hello-mongo/index.js
+++ nodejs/hello-mongo/index.js
@@ -69,6 +69,7 @@ app.put('/users/:id', async (req, res) => {
     res.send(user);
 });
 
-app.delete('/users', async (req, res) => {
-
+app.delete('/users/:id', async (req, res) => {
+    await User.deleteOne({ _id: req.params.id });
+    res.send();
 });
